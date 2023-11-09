886d131e32efcf171dc1b8091355ecc7d6d9f53a
surajjha7428 <143312238+surajjha7428@users.noreply.github.com>
2023-11-09 22:15:56 +0530

    final commit

---
+++ Wrike/Header/header.js
@@ -0,0 +1,9 @@
+// let features = document.querySelectorAll(".sections>#ab")
+// features.addEventListner("click",function(){
+//     window.location.href="../feature/features.html"
+// })
+
+let menu = document.querySelector("#menu")
+menu.addEventListener("click",function(){
+    window.location.href ="../Headmenu/menu.html"
+})
---
+++ Wrike/body.js
@@ -0,0 +1,16 @@
+// let auto = document.querySelector("#auto")
+// auto.addEventListener("click",function(){
+//     window.location.href ="../AUTOMATION/auto.html"
+// })
+
+
+
+
+
+document.getElementById("button").addEventListener("click",function(){
+    document.querySelector(".pop").style.display ="flex";
+})
+
+document.getElementById("close").addEventListener("click",function(){
+    document.querySelector(".pop").style.display="none"
+})
